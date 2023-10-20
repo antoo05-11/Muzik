103754af799272528fb74e3e882b60d60119d6d2
thanhan <antooo5113@gmail.com>
2023-10-21 00:44:26 +0700

    Add music collections boxes to home fragment.

--- app/src/main/java/com/example/muzik/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/muzik/ui/home/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.muzik.R
 import com.example.muzik.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +30,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
