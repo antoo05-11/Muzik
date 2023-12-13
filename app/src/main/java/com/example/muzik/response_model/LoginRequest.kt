d6f243587db23bfa3bc9f9b03463cf8d7bc20821
Nguyễn Đăng Dương <21020757@vnu.edu.vn>
2023-12-13 07:52:28 +0700

    fix login

---
+++ app/src/main/java/com/example/muzik/response_model/LoginRequest.kt
@@ -0,0 +1,10 @@
+package com.example.muzik.response_model
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("username")
+    var username: String,
+    @SerializedName("password")
+    var password: String
+)
