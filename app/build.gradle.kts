6161dcf5e7a27bf972b7b4f7cedb187e3acbc305
antoo05-11 <antooo5113@gmail.com>
2023-12-25 17:50:50 +0700

    Fix Android version bug when pending intent initialized.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,4 +104,6 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     implementation("io.socket:socket.io-client:2.0.0")
+
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
 }
